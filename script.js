cb1f4605b548c234beb7350a2592362f06a43af4
Felipe Hsu <48444671+hsulipe@users.noreply.github.com>
2019-06-17 11:55:37 -0300

    Corrections on presentation methods and Events

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
 var sequence = new Array(),
     indexer = 0, 
-    rodada = 0;
+    rodada = 0,
+    onPlay = false;
 
 const boxes = document.querySelectorAll('.box'),
     buttonPlay = document.querySelector('#play'),
+    buttonReset = document.querySelector('#reset'),
     arrayKeys = [38,39,40,37],
     arrayColors = ['green', 'yellow', 'blue', 'red'];
 
-const getRandom = function(){
-    sequence.push(Math.floor(Math.random()*4));
+
+//################### Boxes ###################//
+const disableBoxes = function (){
+    boxes.forEach(e => { 
+        e.setAttribute('data-enabled', 'disabled');
+    });
 }
 
-const resetSequence = function(){
-    sequence = new Array();
-}
-
-const validadeSequenceEntry = function(entry){
-    return (sequence[indexer] === entry)? true : false;
+const enableBoxes = function (){
+    boxes.forEach(e => { 
+        e.setAttribute('data-enabled', 'enabled');
+    });
 }
 
 async function wait(ms) {
@@ -28,31 +32,48 @@ async function wait(ms) {
 const ShineBox = async function(e){
     var box = boxes[e],
     ClasseAnterior = box.className;
+    box.setAttribute('data-enabled','disabled');
     await wait(200);
     box.className += '-lighter';
     await wait(300);
     box.className = ClasseAnterior;
+    box.setAttribute('data-enabled','enabled');
+}
+
+//################### Sequence ###################//
+const getRandom = function(){
+    sequence.push(Math.floor(Math.random()*4));
+}
+
+const resetSequence = function(){
+    sequence = new Array();
+    document.querySelector('#reset').setAttribute('hidden',true);
+}
+
+const validadeSequenceEntry = function(entry){
+    return (sequence[indexer] === entry)? true : false;
 }
 
 const startSequence = async function(){
+    onPlay = true;
+    disableBoxes();
     if(sequence.length === 0){
         for(var i = 0; i < 4 ; i++)
             getRandom();
     }
+    buttonReset.removeAttribute('hidden');
     for(var i = 0; i < sequence.length ; i++){
         await ShineBox(sequence[i])
     }
-    boxes.forEach( e => {
-        e.setAttribute('data-enabled','enabled'); 
-    });
+    onPlay = false;
+    enableBoxes();
 }
 
+//################### Events ###################//
 const Listener = async function(e){
-    if(e.getAttribute('data-enabled') == 'enabled'){
-        e.setAttribute('data-enabled','disabled');
+    if(e.getAttribute('data-enabled') === 'enabled'  && onPlay === false){
         var element = arrayColors.indexOf(e.id);
         await ShineBox(element);
-        e.setAttribute('data-enabled','enabled');
     }
 }
 
@@ -62,17 +83,30 @@ for(var i = 0; i < boxes.length; i++){
     });
 }
 
-buttonPlay.addEventListener('click', e => { startSequence(); });
+buttonPlay.addEventListener('click', e => { 
+    if(onPlay === false)
+    {
+        startSequence(); 
+    } 
+});
+
+buttonReset.setAttribute('hidden',true);
+buttonReset.addEventListener('click', function() {
+    resetSequence();
+});
 
 document.addEventListener('keydown', async function(e){
     switch(e.keyCode){
         case 38: Listener(boxes[0]);break;
         case 39: Listener(boxes[1]);break;
-        case 40: Listener(boxes[2]);break;
-        case 37: Listener(boxes[3]);break;
+        case 40: Listener(boxes[3]);break;
+        case 37: Listener(boxes[2]);break;
         case 32: 
-            boxes.forEach( e => {e.setAttribute('data-enabled','disabled'); });
-            startSequence(); 
+            if(onPlay === false)
+            {
+                startSequence(); 
+            }
             break;
     }
 });
+
